66af19fafa1b7417a8ea3041616f4e64ae23a90e
phanminhdat11 <phanminhdat01@gmail.com>
2023-10-05 15:28:16 +0700

    design fe for app chatbot

--- angular/src/app/chat/chat.component.ts
+++ angular/src/app/chat/chat.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { Socket } from 'ngx-socket-io';
 import { map } from 'rxjs/operators';
 
@@ -9,20 +10,50 @@ import { map } from 'rxjs/operators';
 })
 export class ChatComponent {
 
-  constructor(private socket: Socket) { }
+  constructor(
+    private socket: Socket,
+    private elementRef: ElementRef
+  ) { }
+  @ViewChild('chatpopup') myForm!: ElementRef;
+  @ViewChild('chatbox') chatbox!: ElementRef;
 
-  showChat = false
+
+
+
+
+
 
   ngOnInit() {
     this.getMessage().subscribe((kq) => {
       console.log(kq);
 
     })
+
+
   }
+  // show(){
+  //   (<HTMLStyleElement>document
+  //     .getElementById('myForm')).style.display = ''
+
+  // }
+
+  show() {
+    const nativeEle = this.myForm.nativeElement;
+    nativeEle.style.display = 'block'
+  }
+  close() {
+    const closeChat = this.myForm.nativeElement;
+    closeChat.style.display = 'none'
+  }
+  closeBox(){
+    const clBox = this.chatbox.nativeElement;
+    clBox.style.display = 'none'
+  }
+
   sendMessage(msg: any) {
     this.socket.emit('message', msg.value);
     console.log(msg.value);
-    
+
 
   }
   getMessage() {
