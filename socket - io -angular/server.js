2c3640af56d2f828b3c5ba3ab4ea79347ed788fd
phanminhdat11 <phanminhdat01@gmail.com>
2023-10-18 21:23:33 +0700

    socket io

---
+++ socket - io -angular/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+// const io = new Server(server);
+
+
+
+// cấu hình giúp angular kết nối với socket
+const io = require('socket.io')(server, {
+    cors: {
+        origin: 'http://localhost:4200',
+        methods: ['GET', 'POST'],
+        allowedHeaders: ['my-custom-header'],
+        credentials: true
+    }
+})
+var list_online = []
+
+io.on('connection', (socket) => {
+    console.log(socket.id);
+    list_online.push(socket.id)
+    socket.on('message', (data)=>{  
+        socket.emit('server', 'Server trả về dữ liệu:' + data);
+    })
+
+    socket.on('disconnect', () => {
+        console.log('da thoat:'+socket.id);
+
+        // xoa nguoi offline ra khoi danh sach online(loai bo han tu ra khoi mang)
+        // + Vi tri trong mang
+        // + Xoa phan tu
+        list_online.splice(list_online.indexOf(socket.id),1)
+      });
+});
+
+server.listen(3001, () => {
+  console.log('listening on *:3001');
+});
